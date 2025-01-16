028cf633705474750106038567a6e6c2d6f597f4
Youssef LACHGAR <ylachgar@marjane.ma>
2025-01-16 14:44:30 +0100

    using the signup method , by the onSubmit method

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,18 +1,46 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+
+  constructor(private authService: AuthService){}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
-  
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
 
+    const email = form.value.email;
+    const password = form.value.password;
+
+    this.isLoading = true;
+    if (this.isLoginMode) {
+      // .....
+    } else {
+      this.authService.signup(email,password)
+      .subscribe(
+        resData => {
+          console.log(resData);
+          this.isLoading = false;
+        },
+        error => {
+          console.log(error);
+          this.isLoading = false;
+        }
+      );
+    }
+    form.reset();
+  }
 }
