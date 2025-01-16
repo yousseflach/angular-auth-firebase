c49dafa30d287e725ee73878f915f977127a48ba
Youssef LACHGAR <ylachgar@marjane.ma>
2025-01-16 16:12:07 +0100

    share the error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
@@ -14,43 +14,61 @@ export interface authResponseData {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  signup(email: string, password: string){
-    return this.http.post<authResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBUZ28W5A-f2TymmFmXSX21mDmM3rSzzpc',
-      {
-        email: email,
-        password: password,
-        returnScureToken: true,
-      }
-    ).pipe(
-      catchError(errorRes => {
-        let errorMessage = 'An unknown error occured!';
-        if(!errorRes.error || !errorRes.error.error){
-          return throwError(errorMessage);
+  signup(email: string, password: string) {
+    return this.http
+      .post<authResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBUZ28W5A-f2TymmFmXSX21mDmM3rSzzpc',
+        {
+          email: email,
+          password: password,
+          returnScureToken: true,
         }
-        switch (errorRes.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = 'this email exists already';
-        }
-        return throwError(errorMessage);
-      })
-    );
+      )
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
-  login(email: string, password: string){
+  login(email: string, password: string) {
     return this.http.post<authResponseData>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBUZ28W5A-f2TymmFmXSX21mDmM3rSzzpc',
       {
         email: email,
-      password: password,
-      returnSecureToken: true
+        password: password,
+        returnSecureToken: true,
       }
+    ).pipe(
+      catchError(this.handleError)
     );
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occured!';
+
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage = 'Nous avons bloqué toutes les demandes provenant de cet appareil en raison d\'une activité inhabituelle. Réessayez plus tard.';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'Les information n\'est pas valide';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'Les information n\'est pas valide';
+        break;
+    }
+
+    return throwError(errorMessage);
   }
 }
